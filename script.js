77329976b2ad403304b0c3bfda90044539715beb
John A. Samuel <98849557+jayo14@users.noreply.github.com>
2023-09-10 01:08:23 +0100

    Source code stage one

---
+++ script.js
@@ -0,0 +1,17 @@
+    //Current date
+const currentDate = new Date();
+
+// Days of the week
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+//Current Day of the Week
+const currentDay = daysOfWeek[currentDate.getUTCDay()];
+
+
+//Current UTC time
+const currentUTCTime = currentDate.toISOString().substr(11, 8);
+
+
+//HTML elements with the data-testid attributes
+document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
+document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime;
